32b70808b69bfce01f67cc1552c0b979bbf60796
Jakobrennan <jakob2011@hotmail.co.uk>
2020-05-07 21:04:42 +0100

    adding before merging

---
+++ listsAndDictionaries/heroesInventory3.py
@@ -0,0 +1,9 @@
+# creating list to hold hero's inventory
+
+inventory = ["sword", "armor", "shield", "toothpick"]
+for item in inventory:
+    print(item)
+
+print("\nyou have " + str(len(inventory)) + "items")
+
+
