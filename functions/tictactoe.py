896a8822c15a473e14b4ae76725142a51d94df46
Jakobrennan <jakob2011@hotmail.co.uk>
2020-05-08 22:39:18 +0100

    pushing up for the night

---
+++ functions/tictactoe.py
@@ -0,0 +1,27 @@
+# instructions
+
+def instructions():
+    """Display game instructions"""
+    print("""
+    welcome to tic tac toe
+    you vs the computer
+
+    enter number between 0 - 8
+    you number corresponds with where you want it places on the board layout
+    below
+
+            0  |  1  |  2
+           ----+-----+----
+            3  |  4  |  5
+           ----+-----+----
+            6  |  7  |  8
+
+    prepare, we are about to start...
+    """)
+
+# main part of the file
+
+print("here are the instructions")
+instructions()
+
+
